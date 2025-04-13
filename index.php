839de8668754def22a7ac842bccc0b074a93a39a
zichy <zichy@zichy.de>
2025-04-13 17:24:38 +0200

    Disable file uploads by default

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 define('USERNAME', 'admin');
 define('PASSWORD', 'admin');
 define('DARKMODE', false);
-define('FILES', true);
+define('FILES', false);
 define('ALLOWEDFILETYPES', array('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'image/webp', 'video/webm', 'audio/webm', 'audio/mpeg', 'audio/ogg', 'audio/mpeg', 'video/mp4', 'video/ogg', 'text/plain', 'text/markdown', 'application/pdf', 'application/zip', 'application/vnd.rar', 'application/x-7z-compressed', 'font/otf', 'font/ttf', 'font/woff2'));
 define('MAXFILESIZE', '10000000');
 
